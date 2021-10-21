4e6b1d6a4bd1af9cfde2897fa2a614a8d92d0314
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-10-21 18:03:58 -0300

    implemented logic for making infite scroll

--- src/components/TrackList/index.tsx
+++ src/components/TrackList/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import { Container, BtnReadMore } from './styles';
+import React, { useCallback, useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import Track, { TrackPropsDTO } from '../../entities/track';
+import Loading from '../loading';
+import { Container } from './styles';
+
+type Selector = {
+  data: Array<any>;
+  error?: boolean;
+};
 
 type TrackListProps = {
   children: React.ReactChild;
-  command: () => void;
+  command: () => boolean;
+  data: TrackPropsDTO[] | Track[];
+  error?: boolean;
 };
 
 function TrackList({ children, command }: TrackListProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(entries => {
+      if (entries.some(entry => entry.isIntersecting)) {
+        if (!loading) {
+          setLoading(true);
+          const res = command();
+          if (!res) setLoading(false);
+          return;
+        }
+        return;
+      }
+    });
+
+    intersectionObserver.observe(document.getElementById('pink')!);
+
+    return () => {
+      intersectionObserver.disconnect();
+      setLoading(false);
+    };
+  }, []);
+
   return (
     <Container>
       <div>{children}</div>
-
-      <BtnReadMore type="button" onClick={() => command()}>
-        veja mais músicas
-      </BtnReadMore>
+      {loading ? <Loading /> : null}
+      <div id="pink"></div>
     </Container>
   );
 }
