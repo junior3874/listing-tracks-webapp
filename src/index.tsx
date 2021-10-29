8a625fda4ef4a1b086f0f1a437ae5ffe38d054ee
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-10-29 11:21:23 -0300

    passing player provider in app

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
+import PlayerProvider from './components/player';
 import store, { persistor } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <PlayerProvider>
+          <App />
+        </PlayerProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
