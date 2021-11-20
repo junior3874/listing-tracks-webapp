30e494d8f0d6a9e2d6c5184b6eaf1114e48f564c
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-11-19 22:21:19 -0300

    removed provider from global scope

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import PlayerProvider from './components/player';
+
 import store, { persistor } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <PlayerProvider>
-          <App />
-        </PlayerProvider>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
